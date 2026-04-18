4ee582135f818e8f4d3d58ff70b528b467befe20
Åsmund Olai Sand-Larsen <asmund.sl@proton.me>
2026-04-18 20:22:28 +0200

    Possibly bad implementation of extracting infix operators

--- implementation/Parser.hs
+++ implementation/Parser.hs
@@ -3,8 +3,72 @@
 
 module Parser where
 
+import Data.Char (isAlphaNum, isControl, isSpace)
+import Data.List (groupBy, sortBy)
 import Syntax
-import Text.Parsec
+import Text.ParserCombinators.Parsec
+
+parseInfixes :: String -> Either ParseError OpTable
+parseInfixes s = do
+    operators <- parse (infixity <* eof) "infixes" s
+    let groups = groupBy grouping operators
+        sortedGroups = map (sortBy sorting) groups
+        withoutPrec = map (map removePrec) sortedGroups
+        extractedInfixity = map extractInfixity withoutPrec
+
+    return $ OpTable $ map extractInfixity withoutPrec
+  where
+    grouping a b = a == b
+    sorting (_, l, _) (_, r, _) = compare l r
+    removePrec (f, _, n) = (f, n)
+    extractInfixity l@((i, _) : _) = (i, map second l)
+    second (_, v) = v
+
+infixity :: Parser [(Fixity, Prec, Op)]
+infixity = otherDef >> sepEndBy binOpDef otherDef
+
+otherDef :: Parser ()
+otherDef = optional $ manyTill anyChar $ char '.' >> spaces
+
+fixity :: Parser (Prec -> Op -> (Fixity, Prec, Op))
+fixity =
+    string "infix"
+        >> ( parseInfix
+                <|> parseInfixL
+                <|> parseInfixR
+           )
+            <* spaces
+  where
+    parseInfix :: Parser (Prec -> Op -> (Fixity, Prec, Op))
+    parseInfix = space >> return (triple Infix)
+    parseInfixL :: Parser (Prec -> Op -> (Fixity, Prec, Op))
+    parseInfixL = char 'l' >> space >> return (triple InfixL)
+    parseInfixR :: Parser (Prec -> Op -> (Fixity, Prec, Op))
+    parseInfixR = char 'r' >> space >> return (triple InfixL)
+    triple :: Fixity -> Prec -> Op -> (Fixity, Prec, Op)
+    triple fst snd trd = (fst, snd, trd)
+
+binOpDef :: Parser (Fixity, Prec, Op)
+binOpDef =
+    fixity
+        <*> (nat <* manyTill alphaNum space <* spaces)
+        <*> binOpName
+        <* manyTill anyChar (char '.')
+        <* spaces
+
+nat :: Parser Int
+nat = zero <|> nonzero
+  where
+    zero = char '0' >> return 0 <* spaces
+    nonzero = read <$> many1 digit <* spaces
+
+binOpName :: Parser (Op)
+binOpName = many1 (satisfy binOpChar) <* spaces
+  where
+    binOpChar c =
+        (not $ isControl c)
+            && (not $ isAlphaNum c)
+            && (not $ isSpace c)
 
 parseCommand :: Prompt -> Either ParseError Command
 parseCommand = undefined
--- implementation/Syntax.hs
+++ implementation/Syntax.hs
@@ -17,59 +17,67 @@ type D = Name -- Datatype Names.
 type Op = Name -- Operator Names.
 
 data Fixity
-  = InfixR -- Right-associative.
-  | InfixL -- Left-associative.
-  | Infix -- Non-associative.
+    = InfixR -- Right-associative.
+    | InfixL -- Left-associative.
+    | Infix -- Non-associative.
+    deriving (Eq, Show)
 
 data Definition
-  = DataDef D [X] [(C, [Type])] -- A data definition.
-  | VarDef Name Type Term -- A variable definition.
+    = DataDef D [X] [(C, [Type])] -- A data definition.
+    | VarDef Name Type Term -- A variable definition.
+    deriving (Eq, Show)
 
 data OpTable
-  = OpTable [(Fixity, [Op])] -- A list of variables, ordered by precedence.
+    = OpTable [(Fixity, [Op])] -- A list of variables, ordered by precedence.
+    deriving (Eq, Show)
 
 data Type
-  = Prim D [Type] -- A primitive type.
-  | Type :->: Type -- The type arrow.
-  | TypeVar X -- A type variable.
+    = Prim D [Type] -- A primitive type.
+    | Type :->: Type -- The type arrow.
+    | TypeVar X -- A type variable.
+    deriving (Eq, Show)
 
 data Pattern
-  = VarPat X -- A variable pattern.
-  | ConPat C [Pattern] -- A construcor pattern.
+    = VarPat X -- A variable pattern.
+    | ConPat C [Pattern] -- A construcor pattern.
+    deriving (Eq, Show)
 
 data Term
-  = Variable Name -- A variable.
-  | Construcor C [Term] -- A constructor.
-  | Application Term Term -- A term applied to a term.
-  | Case Term [(Pattern, Term)] -- A case-term.
-  | Function Name Term -- A function term.
+    = Variable Name -- A variable.
+    | Constructor C [Term] -- A constructor.
+    | Application Term Term -- A term applied to a term.
+    | Case Term [(Pattern, Term)] -- A case-term.
+    | Function Name Term -- A function term.
+    deriving (Eq, Show)
 
 data Value
-  = Value C [Value] -- An algebraic value.
-  | Lambda Name Term -- A function value.
+    = Value C [Value] -- An algebraic value.
+    | Lambda Name Term -- A function value.
+    deriving (Eq, Show)
 
 data Command
-  = Load FilePath -- Load a program.
-  | Reload -- Reload the previous program.
-  | Eval Term -- Evaluate this term.
-  | Help -- Display a help message.
-  | Quit -- Exit the shell.
+    = Load FilePath -- Load a program.
+    | Reload -- Reload the previous program.
+    | Eval Term -- Evaluate this term.
+    | Help -- Display a help message.
+    | Quit -- Exit the shell.
+    deriving (Eq, Show)
 
 data Program
-  = Program
-  { gamma :: X -> Maybe (Type, Term),
-    delta :: D -> Maybe ([X], [(C, [Type])])
-  }
+    = Program
+    { gamma :: X -> Maybe (Type, Term)
+    , delta :: D -> Maybe ([X], [(C, [Type])])
+    }
 
 -- note that patterns and values are a subset of term, so you can have:
 
 class IsTerm thing where
-  toTerm :: thing -> Term
+    toTerm :: thing -> Term
 
 instance IsTerm Pattern where
-  toTerm (VarPat x) = Variable x
-  toTerm (ConPat c ps) = Constructor c (toTerm <$> ps)
+    toTerm (VarPat x) = Variable x
+    toTerm (ConPat c ps) = Constructor c (toTerm <$> ps)
 
 instance IsTerm Value where
-  toTerm (Value c vs) = Constructor c (toTerm <$> vs)
-  toTerm (Lambda x t) = Function x t
+    toTerm (Value c vs) = Constructor c (toTerm <$> vs)
+    toTerm (Lambda x t) = Function x t
