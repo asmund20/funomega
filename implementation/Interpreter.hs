dd5b227f05c131a11b438f367a0f30dc8849a984
Åsmund <asmund.sl@proton.me>
2026-05-08 15:15:12 +0200

    Lots of stuff that makes fib seem to work

--- implementation/Interpreter.hs
+++ implementation/Interpreter.hs
@@ -3,6 +3,7 @@
 module Interpreter (interpretTerm) where
 
 import Control.Monad (liftM)
+
 import GHC.Base (ap)
 import Syntax
 
@@ -42,25 +43,23 @@ abort re = Runtime (const (Left re))
 interpretTerm :: [Definition] -> Term -> Either RuntimeError Value
 interpretTerm ds t = run (eval t) (defListToProgram ds)
 
--- = DataDef D [X] [(C, [Type])] -- A data definition.
--- \| VarDef Name Type Term -- A variable definition.
 defListToProgram :: [Definition] -> Program
 defListToProgram [] = emptyProgram
 defListToProgram ((DataDef d typeVars constructors) : ds) =
-    addDataType d typeVars constructors $ defListToProgram ds
+    addDataType $ defListToProgram ds
   where
-    addDataType :: D -> [X] -> [(C, [Type])] -> Program -> Program
-    addDataType d typeVars constructors prog =
+    addDataType :: Program -> Program
+    addDataType prog =
         prog
             { delta = \y ->
                 if d == y
                     then Just (typeVars, constructors)
                     else delta prog y
             }
-defListToProgram ((VarDef x ty term) : ds) = addVar x ty term $ defListToProgram ds
+defListToProgram ((VarDef x ty term) : ds) = addVar $ defListToProgram ds
   where
-    addVar :: X -> Type -> Term -> Program -> Program
-    addVar x ty term prog =
+    addVar :: Program -> Program
+    addVar prog =
         prog
             { gamma = \y ->
                 if x == y
@@ -71,50 +70,51 @@ defListToProgram ((VarDef x ty term) : ds) = addVar x ty term $ defListToProgram
 emptyProgram :: Program
 emptyProgram = Program (const Nothing) (const Nothing)
 
--- TODO: for fib.fomega, fib S results in a valid program. Should not.
 mgu :: Pattern -> Value -> Maybe Substitution
-mgu (VarPat x) v = Just $ substitute x v
-mgu p@(ConPat cp ps) v@(Value cv vs)
-    -- \| cp == cv && (toTerm p) == (toTerm v) = do
-    --     subs <- mapM (\(p, v) -> mgu p v) (zip ps vs)
-    --     Just $ foldl (.) id subs
-    | cp == cv = do
+mgu (VarPat x) v = Just $ substitute x (toTerm v)
+mgu (ConPat cp ps) (Value cv vs)
+    | cp == cv && length ps == length vs = do
         subs <- mapM (\(p, v) -> mgu p v) (zip ps vs)
         Just $ foldl (.) id subs
+    | otherwise = Nothing
 mgu _ _ = Nothing
 
 {- | p -> v -> term -> term
 | Replace toTerm p with toTerm v in t
 -}
-substitute :: X -> Value -> Term -> Term
-substitute x v = substitute' (Variable x) (toTerm v)
+substitute :: X -> Term -> Term -> Term
+substitute v1 t2 t3@(Variable v3)
+    | v1 == v3 = t2
+    | otherwise = t3
+substitute t1 t2 (Constructor c ts) =
+    Constructor
+        c
+        $ map (substitute t1 t2) ts
+substitute t1 t2 (Application tl tr) =
+    Application
+        (substitute t1 t2 tl)
+        (substitute t1 t2 tr)
+substitute t1 t2 (Case tc cs) = substituteCase t1 t2 (substitute t1 t2 tc) cs
+substitute v1 t2 t3@(Function n tf)
+    | v1 == n = t3
+    | otherwise = Function n $ substitute v1 t2 tf
+
+{- | v1 -> t2 -> tc -> patterns -> Term
+| Replace v1 with t2 in patterns
+-}
+substituteCase :: X -> Term -> Term -> [(Pattern, Term)] -> Term
+substituteCase _ _ tc [] = Case tc []
+substituteCase v1 t2 tc (c@(pat, tp) : cs)
+    | containsVar v1 pat =
+        Case tc $ c : (map (\(p, t) -> (p, substitute v1 t2 t)) cs)
+    | otherwise =
+        Case tc $
+            (pat, substitute v1 t2 tp)
+                : (map (\(p, t) -> (p, substitute v1 t2 t)) cs)
   where
-    -- TODO: funOmega: implementation/Interpreter.hs:(98,5)-(110,73): Non-exhaustive patterns in function substitute'
-    -- Replace t1 with t2 in t3, t1 can only be variable
-    substitute' :: Term -> Term -> Term -> Term
-    substitute t1 t2 t3@(Variable _)
-        | t1 == t3 = t2
-        | otherwise = t3
-    substitute' t1 t2 (Constructor c ts) =
-        Constructor
-            c
-            $ map (substitute' t1 t2) ts
-    substitute' t1 t2 (Application tl tr) =
-        Application
-            (substitute' t1 t2 tl)
-            (substitute' t1 t2 tr)
-    substitute' t1 t2 (Case tc cs) = substituteCase t1 t2 $ Case (substitute' t1 t2 tc) cs
-    substitute' t1@(Variable x) t2 t3@(Function n tf)
-        | x == n = t3
-        | otherwise = Function n $ substitute' t1 t2 tf
-    substitute' t1 t2 (Function n tf) = Function n $ substitute' t1 t2 tf
-    substituteCase t1 t2 t3@(Case _ []) = t3
-    substituteCase t1@(Variable v1) t2 t3@(Case tc (c@((VarPat vc), tp) : cs))
-        | v1 == vc = Case tc $ c : (map (\(p, t) -> (p, substitute' t1 t2 t)) cs)
-        | otherwise =
-            Case tc $
-                ((VarPat vc), substitute' t1 t2 tp)
-                    : (map (\(p, t) -> (p, substitute' t1 t2 t)) cs)
+    containsVar :: X -> Pattern -> Bool
+    containsVar x (VarPat v) = x == v
+    containsVar x (ConPat _ pts) = any (containsVar x) pts
 
 eval :: Term -> Runtime Value
 eval (Variable x) = getVar x >>= eval
@@ -126,38 +126,24 @@ eval (Application t0 t1) = do
     v0 <- eval t0
     case v0 of
         Lambda x t -> do
-            v1 <- eval t1
-            replace x (toTerm v1) t >>= eval
-        _ -> abort "Cannot apply a constructor value to something"
-eval (Case t []) = abort $ "Type checker should make sure this never happens"
+            eval $ substitute x t1 t
+        _ ->
+            abort $
+                "Cannot apply a constructor value to something. Applying  "
+                    ++ show v0
+                    ++ " to "
+                    ++ show t1
+eval (Case _ []) = abort $ "Type checker should make sure this never happens"
 eval (Case t pts@((_, _) : _)) = do
     v <- eval t
     evalCase v pts
-  where
-    evalCase :: Value -> [(Pattern, Term)] -> Runtime Value
-    evalCase v [] = abort $ "None of the patterns matched " ++ show v
-    evalCase v ((pat, term) : pts) = do
-        case mgu pat v of
-            Nothing -> evalCase v pts
-            Just sub -> eval (sub term)
 
-replace :: X -> Term -> Term -> Runtime Term
-replace x t (Variable x')
-    | x' == x = return t
-    | otherwise = return $ Variable x'
-replace x t (Constructor c ts) = do
-    vs <- mapM (replace x t) ts
-    return $ Constructor c vs
-replace x t (Function x' t')
-    | x == x' = return $ Function x' t'
-    | otherwise = replace x t t'
-replace x t (Application t1 t2) = do
-    t1' <- replace x t t1
-    t2' <- replace x t t2
-    return $ Application t1' t2'
-replace x t (Case tc pts) = do
-    tc' <- replace x t tc
-    return $ Case tc' pts
+evalCase :: Value -> [(Pattern, Term)] -> Runtime Value
+evalCase v [] = abort $ "None of the patterns matched " ++ show v
+evalCase v ((pat, term) : pts) = do
+    case mgu pat v of
+        Nothing -> evalCase v pts
+        Just sub -> eval (sub term)
 
 getVar :: X -> Runtime Term
 getVar x =
